d26a9e37f6dc815e7ebb1271dc0c6f6511991470
javante_nelson <javantenelson@gmail.com>
2020-05-11 08:53:37 -0500

    added columns to my tables

--- db/migrate/20200511134818_create_games.rb
+++ db/migrate/20200511134818_create_games.rb
@@ -1,4 +1,9 @@
 class CreateGames < ActiveRecord::Migration
   def change
+    create_table :games do |t|
+      t.string :title
+      t.string :genre
+      t.string :platform 
+    end 
   end
 end
