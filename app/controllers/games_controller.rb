bd8a92dd8d16e349314837f79997709345963b36
javante_nelson <javantenelson@gmail.com>
2020-06-02 09:25:15 -0500

    make code appear cleaner

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,79 +1,56 @@
 class GamesController < ApplicationController 
     
 
-      get "/games" do 
-        redirect_if_not_logged_in
-        @games = current_user.games
-        erb :"/games/index"
-      end 
+    get "/games" do 
+      redirect_if_not_logged_in
+      @games = current_user.games
+      erb :"/games/index"
+    end 
 
-      get "/games/new" do
-        redirect_if_not_logged_in
-        erb :"games/new" 
-      end 
+    get "/games/new" do
+      redirect_if_not_logged_in
+      erb :"games/new" 
+    end 
 
-      post "/games" do
-        redirect_if_not_logged_in
-        @game = current_user.games.build(params)
-        make_game
-      end  
+    post "/games" do
+      redirect_if_not_logged_in
+      @game = current_user.games.build(params)
+      make_game
+    end  
 
-      get "/games/:id/edit" do
-        if logged_in?
-          set_game
-          if @game 
-            erb :"games/edit"
-          else
-            redirect "/games"
-          end
-        else
-          redirect "/login"
-        end 
-      end 
+    get "/games/:id/edit" do
+      redirect_if_not_logged_in
+      game_exist 
+      erb :"games/edit"    
+    end 
 
-      patch "/games/:id" do
-        if logged_in?
-          @game = current_user.games.find_by_id(params[:id])
-          if @game
-            if @game.update(title: params[:title], genre: params[:genre], platform: params[:platform])
-            redirect "/games/#{@game.id}"
-            else
-            redirect "/games/#{@game.id}/edit"
-            end
-          else 
-            redirect "/games"
-          end
-        else
-          redirect "/login"
-        end   
+    patch "/games/:id" do
+      redirect_if_not_logged_in
+      game_exist
+      if @game.update(title: params[:title], genre: params[:genre], platform: params[:platform])
+        redirect "/games/#{@game.id}"
+      else
+        redirect "/games/#{@game.id}/edit"
       end
+    end
 
-      get "/games/:id" do
-        if logged_in?
-          @game = current_user.games.find_by_id(params[:id])
-          if @game 
-            erb :'/games/show'
-          else
-            redirect "/games"
-          end
-        else
-          redirect "/login"
-        end 
-      end
+    get "/games/:id" do
+      redirect_if_not_logged_in
+      game_exist
+      erb :'/games/show'
+    end
       
-      delete "/games/:id" do 
-        if logged_in?
-          set_game
-          if @game 
-            @game.delete
-            redirect "/games"
-          end 
-        else
-          redirect "/login"
-        end    
+    delete "/games/:id" do 
+      redirect_if_not_logged_in
+      set_game
+      if @game 
+        @game.delete
+        redirect "/games"
       end 
+    end 
       
     private
+    
     def set_game
       @game = current_user.games.find_by_id(params[:id])
     end
@@ -84,5 +61,12 @@ class GamesController < ApplicationController
       else 
         redirect "/games/new"
       end    
+    end
+    
+    def game_exist
+      set_game
+      if @game == nil 
+        redirect "/games"
+      end 
     end 
 end 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,41 +1,35 @@
 class UsersController < ApplicationController 
 
    
+   get "/signup" do
+      redirect_if_logged_in 
+      erb :'/users/signup'
+   end 
 
-     get "/signup" do
-        redirect_if_logged_in 
-        erb :'/users/signup'
-     end 
+   post "/signup" do
+      is_it_blank
+      user =User.create(params)
+      redirect "/login"
+   end 
 
-     post "/signup" do
-        is_it_blank
-        user =User.create(params)
-        redirect "/login"
-     end 
+   get "/login" do 
+      redirect_if_logged_in
+      erb :"/sessions/login"
+   end 
 
-     get "/login" do 
-        redirect_if_logged_in
-        erb :"/sessions/login"
-     end 
-
-     post "/login" do
-        user = User.find_by(username: params[:username])
-
-		    if user && user.authenticate(params[:password])
-			    session[:user_id] = user.id
-              redirect "/games"
-		    else
-			      redirect "/login"
-		    end 
-
-     end
+   post "/login" do
+      user = User.find_by(username: params[:username])
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+         redirect "/games"
+		else
+			redirect "/login"
+		end 
+   end
      
-     get "/logout" do 
-        if logged_in?
-          session.clear 
-          redirect "/login"
-        else
-         redirect "/"
-        end 
-      end 
+   get "/logout" do
+      redirect_if_not_logged_in
+      session.clear 
+      redirect "/"
+   end 
 end
