e29240081bac251a23431b5d157669fd2e747531
JavanteS <62562040+JavanteS@users.noreply.github.com>
2020-06-02 09:39:29 -0500

    Update Users Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,12 +19,12 @@ class UsersController < ApplicationController
 
    post "/login" do
       user = User.find_by(username: params[:username])
-		if user && user.authenticate(params[:password])
-			session[:user_id] = user.id
+      if user && user.authenticate(params[:password])
+	 session[:user_id] = user.id
          redirect "/games"
-		else
-			redirect "/login"
-		end 
+      else
+	 redirect "/login"
+      end 
    end
      
    get "/logout" do
